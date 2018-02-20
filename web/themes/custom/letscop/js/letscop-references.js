7a660e8c2375816976a88b1ae784167f610ae43b
Christophe Espiau <christophe.espiau@gmail.com>
2018-02-20 16:39:14 +0100

    Library letscop-references.js added.

---
+++ web/themes/custom/letscop/js/letscop-references.js
@@ -0,0 +1,15 @@
+/**
+ * Created by ubuntu on 20/02/18.
+ */
+
+(function ($) {
+  Drupal.behaviors.letscopReferences = {
+    attach: function (context, settings) {
+      $(document).ready(function(){
+
+      });
+
+
+    }
+  };
+})(jQuery);
